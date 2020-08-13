72544b9a4882b42898b99966a82c7ecbf281fc36
Collin Porter <colls.potta@gmail.com>
2020-08-12 22:19:48 -0600

    added time and score function

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+var scoreUp = 0;
+
 $('div#Q1,#Q2,#gameover').hide();
 
 $("#B1").on("click", function(){
@@ -8,18 +10,28 @@ $("#B1").on("click", function(){
 
 $("#winner1").on("click", function(){
     $("#Q1").hide();$("#Q2").show();
+    scoreUp = scoreUp + 1;
+    $("#scoreboard").html(scoreUp);
 });
 
 $("#loser1,#loser2,#loser3").on("click", function(){
     $("#Q1").hide();$("#Q2").show();
+    secondsLeft = secondsLeft - 10
+    $("#time").html(secondsLeft);
+    scoreUp = scoreUp - 1;
+    $("#scoreboard").html(scoreUp);
 });
 
 $("#winner2").on("click", function(){
     $("#Q2").hide();$("#gameover").show();
+    scoreUp = scoreUp + 1;
+    $("#scoreboard").html(scoreUp);
 });
 
 $("#loser4,#loser5,#loser6").on("click", function(){
     $("#Q2").hide();$("#gameover").show();
+    secondsLeft = secondsLeft - 10
+    $("#time").html(secondsLeft);
 });
 
 $("#restart").on("click", function(){
@@ -27,8 +39,31 @@ $("#restart").on("click", function(){
 });
 
 
+var timeEl = document.querySelector("#time");
+var secondsLeft = 1000;
+var click = document.querySelector("#B1");
 
+click.addEventListener("click",function() {
+    console.log(click)
+  var timerInterval = setInterval(function() {
+      console.log(timerInterval)
+    secondsLeft--;
+    timeEl.innerText = secondsLeft + " seconds left till ";
 
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      sendMessage("Time's Up!");
+    }
+// this is the speed of the clock not the intervals
+  }, 1000);
+})
+console.log(click)
+// setTime();
+// var click = document.querySelector("#answer1");
+// click.addEventListener("click",function answer(){
+// var scoreInterval = setTime
+// });
+// console.log('answer1')
 
 
 
@@ -39,28 +74,3 @@ $("#restart").on("click", function(){
 
 
 });
-
-
-
-// var click = document.querySelector("#tryme");
-// click.addEventListener("click",function setTime() {
-//   var timerInterval = setInterval(function() {
-//     secondsLeft--;
-//     timeEl.textContent = secondsLeft + " seconds left till ";
-//     if (click === 1) {
-//         click.classList.add('hide')
-//     }
-//     if(secondsLeft === 0) {
-//       clearInterval(timerInterval);
-//       sendMessage();
-//     }
-// // this is the speed of the clock not the intervals
-//   }, 1000);
-// })
-// console.log(click)
-// setTime();
-// var click = document.querySelector("#answer1");
-// click.addEventListener("click",function answer(){
-// var scoreInterval = setTime
-// });
-// console.log('answer1')
